0ac3decd97e5c23d7d87e4a414f7ac46e595979d
Kobe Karl M. Sasuman <kobekarl.sasuman@smc.pshs.edu.ph>
2026-01-18 19:49:46 +0800

    Add files via upload

---
+++ Sasuman_Q3_FA02.py
@@ -0,0 +1,9 @@
+study_hours = [
+    [2, 3, 2, 4, 3],
+    [1, 2, 2, 3, 2],
+    [3, 3, 4, 3, 4]]
+print("Mark's study hours on Wednesday:", study_hours[2][2])
+print("My study hours:', study_hours[0]")
+print("Updating my Thursday study hours to 5.")
+study_hours[0][3] = 5
+print("My updated study hours:", study_hours[0])
